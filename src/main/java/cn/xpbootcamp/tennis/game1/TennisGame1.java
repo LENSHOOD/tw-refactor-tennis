7f7c27de5d48c5bc64c43fc946ee326010a1c623
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-03-03 22:59:07 +0800

    refactor(game1): merge point greater than 4

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -9,62 +9,37 @@ public class TennisGame1 extends TennisGame {
 
     @Override
     public String getScore() {
-        String score = "";
         if (isScoreEqual()) {
             return equalScore();
         }
-        if (isScore1GreaterThan4()) {
+
+        if (hasScoreGreaterThan4()) {
             return winnerScore();
         }
-        if (isScore2GreaterThan4()) {
-            return winnerScore();
-        }
 
-        return calculateScore(score);
+        return mergeScore();
     }
 
-    private String calculateScore(String score) {
-        int tempScore;
-        for (int i = 1; i < 3; i++) {
-            if (i == 1) {
-                tempScore = p1point;
-            } else {
-                score += "-";
-                tempScore = p2point;
-            }
-            if (tempScore == 0) {
-                score += "Love";
-            } else if (tempScore == 1) {
-                score += "Fifteen";
-            } else if (tempScore == 2) {
-                score += "Thirty";
-            } else if (tempScore == 3) {
-                score += "Forty";
-            }
-        }
-        return score;
+    private boolean hasScoreGreaterThan4() {
+        return p2point >= 4 || p1point >= 4;
     }
 
-    private String winnerScore() {
-        String score;
+    private String  winnerScore() {
         int minusResult = p1point - p2point;
         if (minusResult == 1) {
-            score = "Advantage player1";
-        } else if (minusResult == -1) {
-            score = "Advantage player2";
-        } else if (minusResult >= 2) {
-            score = "Win for player1";
-        } else {
-            score = "Win for player2";
+            return "Advantage player1";
         }
-        return score;
+        if (minusResult == -1) {
+            return  "Advantage player2";
+        }
+        if (minusResult >= 2) {
+            return  "Win for player1";
+        }
+
+        return "Win for player2";
     }
 
-    private boolean isScore2GreaterThan4() {
-        return p2point >= 4;
-    }
-
-    private boolean isScore1GreaterThan4() {
-        return p1point >= 4;
+    private String mergeScore() {
+        return getScoreByPoint(p1point) + "-" + getScoreByPoint(p2point);
     }
 }
