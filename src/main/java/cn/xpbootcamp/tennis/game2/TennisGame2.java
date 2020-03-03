4681fc114c5585568f42c7b4db09a1a76b9b784d
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-03-03 23:39:15 +0800

    refactor: extract method leadPlayer() to generify logic

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -3,62 +3,31 @@ package cn.xpbootcamp.tennis.game2;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame2 extends TennisGame {
-    private String p1Res = "";
-    private String p2Res = "";
-
     public TennisGame2(String player1Name, String player2Name) {
         super(player1Name, player2Name);
     }
 
     @Override
     public String getScore() {
-        String score = "";
         if (isScoreEqual()) {
-            score = equalScore();
+            return  equalScore();
         }
 
         if (p1point > p2point) {
-            if (p2point == 0) {
-                p1Res = getScoreByPoint(p1point);
-                p2Res = getScoreByPoint(p2point);
-                score = p1Res + "-" + p2Res;
-            }
-            if (p2point >= 3) {
-                score = "Advantage player1";
-            }
-            if (p1point < 4) {
-                p1Res = getScoreByPoint(p1point);
-                p2Res = getScoreByPoint(p2point);
-                score = p1Res + "-" + p2Res;
-            }
-            if (p1point >= 4) {
-                if ((p1point - p2point) >= 2) {
-                    score = "Win for player1";
-                }
-            }
+            return leadPlayer(p1point, p2point, player1Name);
         }
 
-        if (p2point > p1point) {
-            if (p1point == 0) {
-                p2Res = getScoreByPoint(p2point);
-                p1Res = getScoreByPoint(p2point);
-                score = p1Res + "-" + p2Res;
+        return leadPlayer(p2point, p1point, player2Name);
+    }
+
+    private String leadPlayer(int leadPoint, int behindPoint, String playerName) {
+        if (leadPoint >= 4) {
+            if ((leadPoint - behindPoint) >= 2) {
+                return "Win for " + playerName;
             }
-            if (p2point < 4) {
-                p2Res = getScoreByPoint(p2point);
-                p1Res = getScoreByPoint(p1point);
-                score = p1Res + "-" + p2Res;
-            }
-            if (p1point >= 3) {
-                score = "Advantage player2";
-            }
-            if (p2point >= 4) {
-                if ((p2point - p1point) >= 2) {
-                    score = "Win for player2";
-                }
-            }
+            return "Advantage " + playerName;
         }
 
-        return score;
+        return getScoreByPoint(p1point) + "-" + getScoreByPoint(p2point);
     }
 }
